f16b06f5ba6d06e6dc03593923dab23a15c80bd1
Morgrim1986 <andreas.glantschnig@kioto.at>
2014-04-02 19:26:22 +0200

    Update test1.java

--- TestHello/src/test1.java
+++ TestHello/src/test1.java
@@ -1,10 +1,11 @@
-
 public class test1 {
 
 	public static void main(String[] args) {
 	System.out.println("asdf");
 	
 	System.out.println("asdf");
+	System.out.println("ascxxxdf");
+	System.out.println("asxxxxxdf");
 	
 	}
 
